f25e37b1572f615cbfb645b536a01fc69afa4b2a
marka@pobox.com <marka@pobox.com>
2025-05-09 14:41:18 +0000

    Generate correct revision number in a Fossil repository.
    FossilOrigin-Name: 1f3cb071ad6a27420df1e853333e7d60a8e071253abfe6545ddc7f98cbc801f9

--- get_version.rb
+++ get_version.rb
@@ -1,11 +1,23 @@
 #!/usr/bin/env ruby
 
-p = IO::popen(['git', 'rev-parse', 'HEAD'])
-gitver = p.read[0, 7]
-p.close
-File.open("version.cr", "w") do |f|
-  f.puts "module Redwood"
-  f.puts "VERSION = \"git-#{gitver}\""
-  f.puts "end"
+def version
+  if File.exist?('.fslckout')
+    p = IO::popen(['fossil', 'info'])
+    p.each do |line|
+      if line =~ /^checkout:\s*(..........).*$/
+	return "fossil-" + $1
+      end
+    end
+    return "unknown"
+  else
+    p = IO::popen(['git', 'rev-parse', 'HEAD'])
+    return "git-" + p.read[0, 7]
+  end
+  p.close
 end
 
+File.open("version.cr", "w") do |f|
+  f.puts "module Redwood"
+  f.puts "VERSION = \"#{version}\""
+  f.puts "end"
+end
