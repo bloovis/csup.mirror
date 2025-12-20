7d1c03ca38b85326ed904a16c7b7371711752acd
marka <>
2025-12-20 23:04:31 +0000

    Improve docs for keymap.cr.
    FossilOrigin-Name: c3ffa63f83fbe928d748d5fa5164a142936ac21fa63888217ea677daec601d8d

--- src/keymap.cr
+++ src/keymap.cr
@@ -1,13 +1,16 @@
 module Redwood
 
-# We have to use pseudo-global variables for keymaps and global_keymap,
-# because in Crystal, unlike in Ruby, if these were class variables
-# for Mode, they would get a unique instance for each subclass of Mode.
+# `keymaps` is a hash that maps a `Mode` subclass name to
+# the `Keymap` object for that Mode.  In sup, `keymaps` was a class variable for `Mode`.
+# But we can't do that in Crystal, because in Crystal, unlike in Ruby, `keymaps` would
+# get a unique instance for each subclass of `Mode`.
 @@keymaps = Hash(String, Keymap).new
 def self.keymaps
   @@keymaps
 end
 
+# `global_keymap` is the global keymap, i.e. the map for key mappings that
+# are defined at the top level and work in any Mode.
 @@global_keymap : Keymap?
 def self.global_keymap
   @@global_keymap
@@ -16,21 +19,40 @@ def self.global_keymap=(map)
   @@global_keymap = map
 end
 
+# The `Keymap` class represents bindings from key names to actions.  A key can
+# map to an action, or it can map to another `Keymap` object, which allows
+# for commands that require more than one keystroke.  Each Mode creates
+# its own keymap for commands that are specific to that Mode.
 class Keymap
+  # An `Action` is either a method name or another `Keymap`.
   alias Action = String | Keymap
+
+  # An `Entry` records an action, a help string, and one or more key names
+  # that are bound to that action.
   alias Entry = NamedTuple(action: Action, help: String, keynames: Array(String))
 
+  # `map` is a hash that maps a single key name to an `Entry` object.
   property map = Hash(String, Entry).new	# keyname => entry
+
+  # `order` records `Entry` records in the order in which they were defined.
+  # `helplines` uses this to display key binding help in a sensible order,
+  # rather than a semi-random order imposed by the `map` hash table.
   property order = Array(Entry).new
 
+  # The constructor yields `self` so that the passed-in block can add any
+  # bindings it wishes.
   def initialize(&)
     yield self
   end
 
+  # Returns true if the mapping is empty.
   def empty?
     return map.empty?
   end
 
+  # Adds an action to the keymap: *action* is the name of a Mode method,
+  # *help* is the help string for the action, and *keynames* is one or more
+  # key names that are bound to this action.
   def add(action : String | Symbol | Keymap, help : String, *keynames)
     keys = [] of String
     keynames.each {|k| keys << k.to_s}
@@ -49,14 +71,19 @@ class Keymap
     #puts "Added keys #{keynames}, description #{description}, action #{action}, keymap #{self.object_id}, action map #{@map}"
   end
 
+  # Returns true if the keyname *s* is bound to an action.
   def has_key?(s : String)
     @map.has_key?(s)
   end
 
+  # Returns the `Entry` object that is bound to the keyname *s*.
   def [](s : String)
     @map[s]
   end
 
+  # Adds the head entry for a multi-key binding table.  *prompt* is the help string,
+  # and *k* is the keyname that binds to this entry.  `add_multi` calls the passed-in
+  # block with the new `Keymap` object, so that the block can add more key bindings to it.
   def add_multi(prompt : String, k : String | Char)
     kc = k.to_s
     if @map.member? kc
@@ -70,6 +97,7 @@ class Keymap
     end
   end
 
+  # `dump` is a debug-only method that prints the mapping to stdout.
   def dump(int level = 1)
     puts "Keymap dump level #{level}:"
     @map.each do |k, entry|
@@ -85,6 +113,8 @@ class Keymap
     end
   end
 
+  # Looks up the entry for the keyname *c*, and if found, returns
+  # a tuple containing the action and the help string for that entry.
   def action_for(c : String) : Tuple(Action | Nil, String | Nil)
     #puts "action_for: c #{c}, keymap #{self.object_id}, action map #{@map}"
     if has_key?(c)
@@ -95,6 +125,8 @@ class Keymap
     end
   end
 
+  # Returns a set of strings containing all keynames that are
+  # bound to actions in this `Keymap`.
   def keysyms : Set(String)
     s = Set(String).new
     @map.each do |k, entry|
@@ -104,9 +136,13 @@ class Keymap
     return s
   end
 
+  # `KeyHelp` is a tuple with two elements:
+  # * a string that contining all keynames bound to a single action, 
+  #   joined by a comma
+  # * the help string for that action.
   alias KeyHelp = Tuple(String, String)	# {keynames, helpstring}
 
-  # Change an internal keyname to a more user-friendly name
+  # Changes an internal keyname to a more user-friendly name.
   def fix_name(k : String) : String
     case k
     when "C-m"
@@ -120,6 +156,8 @@ class Keymap
     end
   end
 
+  # Returns an array of KeyHelp tuples, each describing a single entry in the keymap.
+  # *except_for* is a set of keynames whose entries must be excluded from the result.
   def help_lines(except_for = Set(String).new, prefix="") : Array(KeyHelp)
     lines = Array(KeyHelp).new
     @order.each do |entry|
@@ -139,15 +177,21 @@ class Keymap
     return lines
   end
 
+  # Returns a nicely formatted set of lines describing the key bindings for each
+  # action in the keymap.  Each line is composed of two columns:
+  # * The list of keynames, joined by commas, and padded with spaces so that the
+  #   second column is aligned properly.
+  # * The help string for the action.
   def help_text(except_for=Set(String).new) : String
     lines = help_lines except_for
     llen = lines.max_of { |kh| kh[0].size }
     lines.map { |a, b| sprintf " %#{llen}s : %s", a, b }.join("\n")
   end
 
-  # Load user-defined key bindings from keymap.yaml.
+  # Loads user-defined key bindings from `~/.csup/keymap.yaml`.
   # The file looks like this:
-  #   modename1:
+  # ```
+  #  modename1:
   #     action1:
   #       - key1
   #       - key2
@@ -156,9 +200,10 @@ class Keymap
   #       [...]
   #  modename2:
   #    [...]
+  # ```
   #
   # The actions must already be defined and have default key bindings,
-  # but keymap.yaml can overwrite or add to those bindings.
+  # but `keymap.yaml` can overwrite or add to those bindings.
   def self.load_keymap
     base_dir   = File.join(ENV["HOME"], ".csup")
     keymap_fn  = File.join(base_dir, "keymap.yaml")
@@ -217,7 +262,7 @@ class Keymap
     end
   end
 
-  # Return the yaml representation of this keymap.
+  # Returns the yaml representation of this keymap.
   def yaml(mode : String) : String
     yaml = "#{mode}:\n"
     @order.each do |entry|
@@ -230,7 +275,7 @@ class Keymap
     return yaml
   end
 
-  # Return the yaml representation of all keymaps, including
+  # Returns the yaml representation of all keymaps, including
   # the global keymap.
   def self.keymaps_to_yaml : String
     yaml = ""
