5642d630853a4aa1272e1758be7759dc8c5e565f
marka <>
2025-12-20 21:38:21 +0000

    Improve docs for horizontal_selector.cr.
    FossilOrigin-Name: d92b9e24ee349355cb68edbdb061ce49c34d803dc1d5ba9f1abcee7d5a009148

--- src/horizontal_selector.cr
+++ src/horizontal_selector.cr
@@ -3,6 +3,9 @@ require "./util.cr"
 
 module Redwood
 
+# `HorizontalSelector` represents a line showing a multi-value selector.
+# The user can select one of the values by moving the cursor left or right.
+# This is used by two Modes: `EditMessageMode` and `ReplyMode`.
 class HorizontalSelector
   class UnknownValue < StandardError; end
 
@@ -10,6 +13,8 @@ class HorizontalSelector
   property changed_by_user : Bool
   property selection : Int32
 
+  # Create a selector whose values are *vals*, with the corresponding
+  # labels *labels*.
   def initialize(@label, vals : Array(String), labels : Array(String), 
 		 base_color=:horizontal_selector_unselected_color,
 		 selected_color=:horizontal_selector_selected_color)
@@ -21,6 +26,9 @@ class HorizontalSelector
     @changed_by_user = false
   end
 
+  # Makes the value *val* the currently selected value.  Raises
+  # an `UnknownValue` exception if *val* is not one of the possible
+  # values for this selector.
   def set_to(val)
     if i = @vals.index(val)
       @selection = i
@@ -29,12 +37,15 @@ class HorizontalSelector
     end
   end
 
+  # Returns true if the current value can be set to *val*.
   def can_set_to?(val)
     !@vals.index(val).nil?
   end
 
+  # Returns the currently selected values.
   def val; @vals[@selection] end
 
+  # Constructs the line displaying the current state of the selector.
   def line(width=nil) : WidgetArray
     label =
       if width
@@ -57,11 +68,15 @@ class HorizontalSelector
     return l
   end
 
+  # Moves the current selection to the left by one, or to the last
+  # if already at the first.
   def roll_left
     @selection = (@selection - 1) % @labels.size
     @changed_by_user = true
   end
 
+  # Moves the current selection to the right by one, or to the first
+  # if already at the last.
   def roll_right
     @selection = (@selection + 1) % @labels.size
     @changed_by_user = true
