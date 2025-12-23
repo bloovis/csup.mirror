3a222d5240e08d1dea3b0420f5c1c431bfed8588
marka <>
2025-12-23 16:11:16 +0000

    Improve docs for notmuch.cr.
    FossilOrigin-Name: 25cf821d1826c3f7c75b7c5f3b841ad6bc8adde6c4f46279da0654f7bc7df8fc

--- src/notmuch.cr
+++ src/notmuch.cr
@@ -7,12 +7,18 @@ require "./logger"
 
 module Redwood
 
+# `Notmuch` is a module (effectively, a singleton class) that provides
+# the interface to the Notmuch mailstore.  It includes methods for
+# searching and tagging messages, extracting messages parts,
+# and polling for new messages.
 module Notmuch
   class ParseError < Exception
   end
 
-  # Completion prefixes for search command.
   COMPL_OPERATORS = %w(and or not)
+
+  # Completion prefixes for the search command, i.e., the reserved
+  # words in the Notmuch query language, plus the boolean operators
   COMPL_PREFIXES =
     %w(
       body from to subject attachment mimetype tag id thread path folder
@@ -22,27 +28,44 @@ module Notmuch
 
   extend self
 
-  # low-level
+  # Low-level methods.
 
+  # Returns the lastmod counter (the counter for number of database updates).
+  # Use this to determine if the mailstore has changed since the last call
+  # to `lastmod`.
   def lastmod : Int32
     Pipe.run("notmuch", ["count", "--lastmod"]).split.last.to_i
   end
 
+  # Polls for new messages added to the mailstore.
   def poll
     Pipe.run("notmuch", ["new", "--quiet"], check_stderr: false)
   end
 
+  # Returns the number of messages in the mailstore that match the *query*.
+  # If query is the empty string (the default), returns the number
+  # of all messages in the mailstore.
   def count(query : String = "")
     Pipe.run("notmuch", ["count", query]).to_i
   end
 
+  # Performs a search and returns the output as an array of lines.
+  # See `man notmuch-search` for details.
+  # Arguments:
+  # * *query*: the query string.
+  # * *format*: specifies the desired output format: "json" or "text" (default).
+  # * *exclude*: true if deleted or spam messages should be excluded
+  #   from the results (default false).
+  # * *output*: specifies the kind of output: "threads" (default), "files", or "messages".
+  # * *offset*: the number of results to skip (default 0).
+  # * *limit*: the maximum number of results to return (default unlimited).
+  # is true
   def search(query : String,
 	     format : String = "text",
 	     exclude : Bool = true,
 	     output : String = "threads",
 	     offset : (Int32 | Nil) = nil,
 	     limit : (Int32 | Nil) = nil) : Array(String)
-    # search threads, return thread ids
     args = ["search", "--format=#{format}", "--output=#{output}"]
     args << "--offset=#{offset}" if offset
     args << "--limit=#{limit}" if limit
@@ -53,6 +76,14 @@ module Notmuch
     Pipe.run("notmuch", args).lines
   end
 
+  # Returns the JSON representation for the messages matching a query.  See
+  # `man notmuch-show` for details. Arguments:
+  # * *query*: the search query; this is typically a list of thread IDs but can
+  #   include other queries.
+  # * *body*: true if the bodies of the messages should be included in
+  #   the results (default false).
+  # * *html*: true if "text/html" message parts should be included in
+  #   the results (default false).
   def show(query : String,
 	   body : Bool = false,
 	   html : Bool = false) : JSON::Any
@@ -62,13 +93,11 @@ module Notmuch
 			check_stderr: false))
   end
 
-  def tag(query : String)
-    Pipe.run("notmuch", ["tag", query])
-  end
-
-  # Each entry in query_tags is a tuple containing:
-  # - a query string in the form "id:messageid"
-  # - an array of tags to apply to that message
+  # Sets the tags for one or more messages, removing any pre-existing tags
+  # for those messages.  See `man notmuch-tag` for details.
+  # *query_tags* is an array of tuples, each of which is in this form:
+  # * a query string in the form "id:messageid"
+  # * an array of tags to apply to that message
   def tag_batch(query_tags : Array({String, Array(String)}))
     return if query_tags.empty?
     input = query_tags.map do |q, ls|
@@ -79,7 +108,10 @@ module Notmuch
     Pipe.run("notmuch", ["tag", "--remove-all", "--batch"], input: input, check_stderr: false)
   end
 
-  # Save a message part to a file.
+  # Saves a message part to a file.  Arguments:
+  # * *msgid*: the message ID
+  # * *partid*: the part ID
+  # * *filename*: name of the output file
   def save_part(msgid : String, partid : Int32, filename : String) : Bool
     if File.exists?(filename)
       unless BufferManager.ask_yes_or_no "File \"#{filename}\" exists. Overwrite?"
@@ -105,9 +137,12 @@ module Notmuch
     return true
   end
 
-  # Read the specified message part into a pipe, and yield the pipe to
-  # the passed-in block.  The block should do an IO.copy to save the
-  # message part. This will be used to attach a file to an EMail::Message.
+  # Writes a message part to a pipe, and yields the pipe to
+  # the passed-in block.  The block should do an `IO#copy` to save the
+  # message part. This is used to attach a file to an `EMail::Message`.
+  # Arguments:
+  # * *msgid*: the message ID
+  # * *partid*: the part ID
   def write_part(msgid : String, partid : Int32) : Bool
     #STDERR.puts "About to run notmuch show --part=#{partid} id:#{msgid}"
     pipe = Pipe.new("notmuch", ["show", "--part=#{partid}", "id:#{msgid}"])
@@ -119,6 +154,15 @@ module Notmuch
     return true
   end
 
+  # Extracts a message part and passes it to the `mime-view` hook.  Returns
+  # true if the hook was run successfully.
+  #
+  # The `mime-view` hook (a script that lives in `~/.csup/hooks`) will
+  # receive a line containing the MIME content type, followed by the raw
+  # data of the message part.  The hook can choose to display the data
+  # in an external program, or it can ignore the data.  Arguments:
+  # * *msgid*: the message ID
+  # * *partid*: the part ID
   def view_part(msgid : String, partid : Int32, content_type : String) : Bool
     #STDERR.puts "view_part: notmuch show --part=#{partid}  id:#{msgid}"
     pipe = Pipe.new("notmuch", ["show", "--part=#{partid}", "id:#{msgid}"])
@@ -137,13 +181,14 @@ module Notmuch
     return success
   end
 
-  # high-level
+  # High-level methods.
 
-  def filenames_from_message_id(mid : String)
+  # Returns a list of filenames corresponding to the message whose ID is *mid*.
+  def filenames_from_message_id(mid : String) : Array(String)
     search("id:#{mid}", exclude: false, format: "text", output: "files", limit: 1)
   end
 
-  # Return thread id for the given message id, or empty string if not found.
+  # Returns the thread ID for message whose ID is *mid*, or the empty string if not found.
   def thread_id_from_message_id(mid : String) : String
     lines = search("id:#{mid}", exclude: false, format: "text", output: "threads", limit: 1)
     if lines.size > 0
@@ -153,23 +198,25 @@ module Notmuch
     end
   end
 
-  def tags_from_message_id(mid : String)
+  # Returns the list of tags for the message whose ID is *mid*.
+  def tags_from_message_id(mid : String) : Array(String)
     search("id:#{mid}", exclude: false, output: "tags")
   end
 
-  # It's too expensive to search notmuch for addresses, so just return an empty list.
+  # Searches notmuch for a list of all possible email addresses.  This turns out to be
+  # too expensive for any non-trivial mailstore, so just return an empty list.
   def load_contacts(email_addresses : Array(String), limit : Int32 = 20)
     return Array(Person).new
   end
 
-  # Translate a query string from the user into one that can
-  # be passed to notmuch search.  Translations include:
-  # - to:/from:person -> look up person's email address contacts list
-  # - label:/is:/has: -> tag:
-  # - filename: -> attachment:
-  # - filetype: -> mimetype:
-  # - before|on|in|during|after: -> date:?..?
-
+  # Translates a query string from the user into one that can
+  # be passed to `notmuch search`.  This is used to provide compatibility
+  # with the query language used in sup.  Translations include:
+  # * to:/from:person -> look up person's email address contacts list
+  # * label:/is:/has: -> tag:
+  # * filename: -> attachment:
+  # * filetype: -> mimetype:
+  # * before|on|in|during|after: -> date:?..?
   def translate_query(s : String) : String
     subs = ""
 
@@ -251,12 +298,12 @@ module Notmuch
     return subs
   end
 
-  # Run "notmuch insert" to store a message in the specified
-  # folder (which can be nil).  Supply a block that takes
-  # an IO object; the block should write the message contents
-  # to the IO.  Return true if successful, or false otherwise.
-
-  def insert(folder, &block : IO -> _) : Bool	# supply a block that takes an IO
+  # Runs `notmuch insert` to store a message in the
+  # folder named *folder* (which can be nil).  The caller supplies a block that takes
+  # an `IO` object; the block should write the message contents
+  # to the IO.  Returns true if successful, or false otherwise.
+  # This is used to save drafts or sent messages to the mailstore.
+  def insert(folder, &block : IO -> _) : Bool
     cmd = "notmuch"
     args = ["insert"]
     if folder
@@ -281,8 +328,8 @@ module Notmuch
     return true
   end
 
-  # Run "notmuch search --output=tags '*'" to get a list of all tags.
-
+  # Runs `notmuch search --output=tags '*'` to get a list of all tags
+  # for all messages, and returns the list as an array of strings.
   def all_tags : Array(String)
     ret = [] of String
     cmd = "notmuch"
