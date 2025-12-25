3c012c10b21fd9cd23bad8d1e8eba6709c589adc
marka <>
2025-12-25 17:00:22 +0000

    Improve docs for singleton.cr.
    FossilOrigin-Name: b0c8210eb5602af65245eb41ff63b7c45acc0bca58c66e7e3becf6606fb8d3ba

--- src/singleton.cr
+++ src/singleton.cr
@@ -1,6 +1,10 @@
-# Macros to simulate the Singleton class used in Sup.
-
-# Use this macro at the start of the class.
+# `singleton_class` is a macro that simulates the Singleton class used in Sup.
+# Use this macro at the start of the class.  It declares the single instance
+# of the class, and defines methods for accessing, deinitializing, and querying
+# the existence of this instance. Use the `singleton_pre_init` and
+# `singleton_post_init` macros in the class `initialize` method.  Use
+# the `singleton_method` macro to define methods that must be bound
+# to the single instance of the class.
 macro singleton_class
   CLASSNAME = {{@type.stringify}}
   @@instance : {{@type}}?
@@ -23,12 +27,14 @@ macro singleton_class
   end
 end
 
-# Use this macro at the beginning of the initialize method.
+# Use this macro at the beginning of the `initialize` method.  It checks
+# that the single instance of this class has not been created already.
 macro singleton_pre_init
     raise self.class.name + " : only one instance can be created" if @@instance
 end
 
-# Use this macro at the end of the initialize method.
+# Use this macro at the end of the `initialize` method.  It creates
+# the single instance of this class.
 macro singleton_post_init
     @@instance = self
 end
