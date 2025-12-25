eefbb0f0f82d835b8eb305a30fb7626326d3844c
marka <>
2025-12-25 15:29:07 +0000

    Improve docs for sent.cr.
    FossilOrigin-Name: 5475f01675159331773c6ee4b3d4fe0402fd76d445d7e2cb23c3b4b04ef274d5

--- src/sent.cr
+++ src/sent.cr
@@ -3,17 +3,25 @@ require "./notmuch"
 
 module Redwood
 
+# `SentManager` is a singleton class whose sole purpose is
+# to save a sent message in the Notmuch mailstore.
 class SentManager
   singleton_class
 
   property folder = ""
 
+  # Saves the Notmuch folder name for sent messages, which is "sent"
+  # by default, but can be overridden by the `:sent_folder` value in
+  # `~/.csup/config.yaml`.
   def initialize(folder : String)
     singleton_pre_init
     @folder = folder
     singleton_post_init
   end
 
+  # Writes a message to the Notmuch "sent" folder.  Passes
+  # an `IO` object to the block, to which the block must write the
+  # contents of the message.
   def self.write_sent_message(&block : IO -> _) : Bool
     stored = false
     ##::Thread.new do
@@ -22,9 +30,6 @@ class SentManager
     ##end #Thread.new
     stored
   end
-  #def self.write_sent_message(&block) : Bool
-  #  self.instance.do_write_sent_message(&block)
-  #end
 
 end # class
 
