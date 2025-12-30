e62d94f8ce17e8d9d495f5b603c990c851a15668
marka <>
2025-12-30 15:17:00 +0000

    Improve docs for widget.cr.
    FossilOrigin-Name: 2cfe23f00be54b6fd37a9c65604bd66292387c7d697c7384e8f52d51afa49e0f

--- src/widget.cr
+++ src/widget.cr
@@ -7,9 +7,23 @@
 
 module Redwood
 
+# A `Widget` is a tuple containg a string and the color that
+# should be applied to that string.
 alias Widget = Tuple(Symbol, String)	# {color, text}
+
+# A line of text can be an array of `Widget` objects.  This
+# is used for text lines that contain strings of diferent colors.
 alias WidgetArray = Array(Widget)
+
+# A line of text is either a string with a single default color,
+# or an array of `Widget` objects, each of which is a string
+# with its own color.
 alias Text = WidgetArray | String
+
+# Lines of text as represented in all Modes derived from `ScrollMode`.
+# Each line is either a string with a single default color,
+# or an array of `Widget` objects, each of which is a string
+# with its own color.
 alias TextLines = Array(Text)
 
 end
