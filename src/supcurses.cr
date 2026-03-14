2382930e0faf3d48239c3345c6fa120f5fd70e9f
marka <>
2026-03-14 00:06:25 +0000

    Remove redundant key definitions from LibNCurses.
    FossilOrigin-Name: 1b1f7b0f5487564cd27b8f534337be96523cd6980478c8d841e87553a77bec24

--- src/supcurses.cr
+++ src/supcurses.cr
@@ -9,48 +9,6 @@ lib LibNCurses
   alias Wint_t = Int32
   type Window = Void*
 
-  # Background values
-  A_NORMAL = 0x0
-  A_REVERSE = 0x40000
-
-  # Keys
-  KEY_CANCEL = 0x163
-  KEY_CODE_YES = 0x100
-  KEY_MOUSE = 0x199
-  KEY_ENTER = 0x157
-  KEY_BACKSPACE = 0x107
-  KEY_UP = 0x103
-  KEY_DOWN = 0x102
-  KEY_LEFT = 0x104
-  KEY_RIGHT = 0x105
-  KEY_PPAGE = 0x153
-  KEY_NPAGE = 0x152
-  KEY_HOME = 0x106
-  KEY_END = 0x168
-  KEY_IC = 0x14b
-  KEY_DC = 0x14a
-  KEY_F1 = 0x109
-  KEY_F2 = 0x10a
-  KEY_F3 = 0x10b
-  KEY_F4 = 0x10c
-  KEY_F5 = 0x10d
-  KEY_F6 = 0x10e
-  KEY_F7 = 0x10f
-  KEY_F8 = 0x110
-  KEY_F9 = 0x111
-  KEY_F10 = 0x112
-  KEY_F11 = 0x113
-  KEY_F12 = 0x114
-  KEY_F13 = 0x115
-  KEY_F14 = 0x116
-  KEY_F15 = 0x117
-  KEY_F16 = 0x118
-  KEY_F17 = 0x119
-  KEY_F18 = 0x11a
-  KEY_F19 = 0x11b
-  KEY_F20 = 0x11c
-  KEY_RESIZE = 0x19a
-
   @[Flags]
   enum Mouse : LibC::ULong
     Position = 0o2_000_000_000
